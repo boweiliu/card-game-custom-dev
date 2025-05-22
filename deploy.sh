c25899df978db396f5c03bdecbda6834fc7a6f14
Bowei Liu <liubowei@gmail.com>
2025-05-22 16:43:12 -0700

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
+# run locally
+
 git rev-parse HEAD > git-HEAD
-
 docker build . --tag card-game-custom-dev
-
 docker run --rm -d -p 3333:3001 card-game-custom-dev
+
+
+# run on fly.io : just push to main
+# https://card-game-custom-dev.fly.dev/
